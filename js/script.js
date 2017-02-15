9ae2be5c03f3b3fd343bdac66bbc2e1fcf526e9a
Your Name <you@example.com>
2017-02-15 14:08:36 +0200

    Maked javascriptClock

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector('.clock-sec');
+const minsHand = document.querySelector('.clock-min');
+const hourHand = document.querySelector('.clock-hour');
+
+function setDay() {
+    const now = new Date();
+
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDay, 1000);
